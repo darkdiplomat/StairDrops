c1108a9d4ac6463ecd92f1787a5d8ac1550b9f24
darkdiplomat <nintendofreck@yahoo.com>
2011-09-24 20:04:16 -0500

    added Nether BrickStairs
    Signed-off-by: darkdiplomat <nintendofreck@yahoo.com>

--- StairDrops/src/StairDropsListener.java
+++ StairDrops/src/StairDropsListener.java
@@ -79,6 +79,25 @@ public class StairDropsListener extends PluginListener {
 						return true;
 					}
 				}
+			}else if (block.getType() == 112){//Nether Brick Stairs
+				if (etc.getLoader().getPlugin("Realms")!=null) {
+					if (etc.getLoader().getPlugin("Realms").isEnabled()) {
+						boolean permission = (Boolean)etc.getLoader().callCustomHook("Realms-PermissionCheck", new Object[] {"destroy", player, block});
+						if (permission){
+							if (getItemCanBreak(iih)){
+								etc.getServer().getWorld(w).dropItem(block.getX(), block.getY(), block.getZ(), 109);
+								block.setType(0);
+								block.update();
+								return true;
+							}
+						}
+					}else if (getItemCanBreak(iih)){
+						etc.getServer().getWorld(w).dropItem(block.getX(), block.getY(), block.getZ(), 109);
+						block.setType(0);
+						block.update();
+						return true;
+					}
+				}
 			}
 		}
 		return false;
